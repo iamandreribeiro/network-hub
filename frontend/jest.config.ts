5d53f841a210700079e28ef373dbb864af4097c7
iamandreribeiro <andreribeiroorafael@gmail.com>
2025-11-10 01:25:54 -0300

    build: jest config

---
+++ frontend/jest.config.ts
@@ -0,0 +1,16 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Forneça o caminho para o seu projeto Next.js
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
