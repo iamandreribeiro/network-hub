7c1464e20a1f1b3c3ec1242d0b1ee4ba01e10fd6
iamandreribeiro <andreribeiroorafael@gmail.com>
2025-11-10 00:29:19 -0300

    feat: importing mailer lib

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IntentionsModule } from './intentions/intentions.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { MembersModule } from './members/members.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [IntentionsModule, PrismaModule],
+  imports: [
+    IntentionsModule,
+    PrismaModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        port: Number(process.env.MAIL_PORT || '587'),
+        secure: process.env.MAIL_SECURE === 'true',
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        }
+      }
+    }),
+    MembersModule,
+    DashboardModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
